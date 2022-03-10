de36353f43424926b98b04c69c94ca75b2d75142
Johan Ablett-Karlsson <jak@radiosphere.com>
2022-03-10 14:17:07 +0100

    Remove unnecessary code

--- src/main/kotlin/io/radiosphere/CounterState.kt
+++ src/main/kotlin/io/radiosphere/CounterState.kt
@@ -1,15 +1,5 @@
 package io.radiosphere
 
-import org.infinispan.protostream.annotations.ProtoFactory
 import org.infinispan.protostream.annotations.ProtoField
 
-data class CounterState(@get:ProtoField(number = 1) var index: Long? = null) {
-
-//    @ProtoFactory
-//    constructor(index: Long?) : this() {
-//        this.index = index
-//    }
-
-//    @get:ProtoField(number = 1)
-//    var index: Long? = 0
-}
+data class CounterState(@get:ProtoField(number = 1) var index: Long? = null)

38486989396ef20cbdc9c62a014767e510647b6d
Johan Ablett-Karlsson <jak@radiosphere.com>
2022-04-04 11:46:37 +0200

    Add UUID to test marshall generation of more complex types

--- src/main/kotlin/io/radiosphere/CounterState.kt
+++ src/main/kotlin/io/radiosphere/CounterState.kt
@@ -1,5 +1,9 @@
 package io.radiosphere
 
 import org.infinispan.protostream.annotations.ProtoField
+import java.util.*
 
-data class CounterState(@get:ProtoField(number = 1) var index: Long? = null)
+data class CounterState(
+    @get:ProtoField(number = 1) var index: Long? = null,
+    @get:ProtoField(number = 2) var uuid: UUID? = null
+)
